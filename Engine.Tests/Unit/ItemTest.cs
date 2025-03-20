a1a5a3304d4fec0b019ec5dff2ac9413fbe1dbb2
Fred George <fredgeorge@acm.org>
2025-03-20 15:49:51 +0530

    Fred: Added multiple choice

--- Engine.Tests/Unit/ItemTest.cs
+++ Engine.Tests/Unit/ItemTest.cs
@@ -4,8 +4,10 @@
  * @author Fred George  fredgeorge@acm.org
  */
 
+using System.Threading;
 using Engine.Checklists;
 using static Engine.Checklists.ItemStatus;
+using static Engine.Tests.Unit.Choices;
 
 using Xunit;
 
@@ -24,4 +26,20 @@ public class ItemTest {
         item.Reset();
         Assert.Equal(Unknown, item.Status());
     }
+
+    [Fact]
+    public void MultipleItemCheck() {
+        var item = new MultipleChoiceItem(A, B, C);
+        Assert.Equal(Unknown, item.Status());
+        item.Be(A);
+        Assert.Equal(Happy, item.Status());
+        item.Be(D);
+        Assert.Equal(Sad, item.Status());
+        item.Reset();
+        Assert.Equal(Unknown, item.Status());
+    }
+}
+
+internal enum Choices {
+    A, B, C, D
 }
---
+++ Engine/Checklists/MultipleChoiceItem.cs
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George  fredgeorge@acm.org
+ */
+
+using static Engine.Checklists.ItemStatus;
+
+namespace Engine.Checklists;
+
+// Understands selecting from a limited list
+public class MultipleChoiceItem {
+    private readonly List<object> _choices;
+    private object? _value;
+
+    public MultipleChoiceItem(params object[] choices) {
+        _choices = choices.ToList();
+    }
+
+    public ItemStatus Status() {
+        if (_value == null) return Unknown;
+        return _choices.Contains(_value) ? Happy : Sad;
+    }
+
+    public void Be(object value) {
+        _value = value;
+    }
+
+    public void Reset() => _value = null;
+}
